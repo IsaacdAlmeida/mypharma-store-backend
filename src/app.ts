ee626f9923314238bd5570a2d1a902e4e8471dd0
Isaac Almeida <isaacalmeidafilho@gmail.com>
2023-04-02 11:20:18 -0300

    habilita cors

--- src/app.ts
+++ src/app.ts
@@ -1,9 +1,11 @@
 import express from 'express';
 import 'express-async-errors';
+import cors from 'cors';
 import errorHandler from './middlewares/error';
 import productRoute from './routes/products.route';
 
 const app = express();
+app.use(cors());
 app.use(express.json());
 app.use(productRoute);
 app.use(errorHandler);
